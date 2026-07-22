c0863a8e430a241e437c7d6283bec1dc42a266ea
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-07-21 22:31:46 -0300

    adiciona script para combinar estoques

---
+++ estoque.py
@@ -0,0 +1,6 @@
+estoque1 = tuple(input('Digite os produtos do estoque 1 (separados por vírgula): ').split(', '))
+estoque2 = tuple(input('Digite os produtos do estoque 2 (separados por vírgula): ').split(', '))
+
+estoque_combinado = estoque1 + estoque2
+
+print(f'Estoque combinado:\n{estoque_combinado}')
