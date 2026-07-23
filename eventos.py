28367174b3887ad0f7a9f7048125c934a0f92a98
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-07-22 23:20:53 -0300

    adiciona script para reverter a ordem dos eventos registrados

---
+++ eventos.py
@@ -0,0 +1,4 @@
+eventos_registrados = ['Encerramento', 'Palestra 3', 'Palestra 2', 'Abertura']
+print(f'Ordem anterior: {eventos_registrados}')
+eventos_registrados.reverse()
+print(f'Ordem corrigida: {eventos_registrados}')
